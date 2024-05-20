fd2c7b012b31c2c43a18fba1e56f05fa749ff1d6
Qubi00 <jason1004qiu@gmail.com>
2024-05-20 17:16:00 -0400

    created lccn and state filter, updated navbar

--- vite-project/src/components/table/table.jsx
+++ vite-project/src/components/table/table.jsx
@@ -3,35 +3,55 @@ import './table.css'
 
 function Table(){
     const [newsData, setNewsData] = useState([]);
- 
+    const [stateFilter, setStateFilter] = useState('');
+    const [lccnFilter, setLccnFilter] = useState('');
+
     useEffect(() => {
         const corsProxy = 'https://cors-anywhere.herokuapp.com/';
         const url = 'https://chroniclingamerica.loc.gov/newspapers.json';
+        
         fetch(corsProxy + url)
             .then(response => response.json())
             .then(data => setNewsData(data.newspapers));
     }, []);
+
+    const filteredData = newsData.filter((item) => 
+        (item.state && item.state.toLowerCase().includes(stateFilter.toLowerCase())) &&
+    (item.lccn && item.lccn.toLowerCase().includes(lccnFilter.toLowerCase()))
+    );
 return (
-        <table>
-            <thead>
+    <div className="tableSearch">
+    <input
+      className="lccn-search"
+      placeholder="Search by Lccn"
+      onChange={(e) => setLccnFilter(e.target.value.toLowerCase())}
+    />
+    <input
+      className="state-search"
+      placeholder="Search by State"
+      onChange={(e) => setStateFilter(e.target.value.toLowerCase())}
+    />
+        <table className = "table">
+            <thead className = "header">
                 <tr>
-                    <th>Iccn</th>
+                    <th>Lccn</th>
                     <th>Url</th>
                     <th>State</th>
                     <th>Title</th>
                 </tr>
             </thead>
             <tbody>
-                {newsData.map((item, id) => (
+                {filteredData.map((item, id) => (
                     <tr key={id}>
-                        <td>{item.iccn}</td>
-                        <td>{item.url}</td>
+                        <td>{item.lccn}</td>
+                        <td><a href={item.url}>{item.url}</a></td>
                         <td>{item.state}</td>
                         <td>{item.title}</td>
                     </tr>
                 ))}
             </tbody>
         </table>
+    </div>
     );
 }
 
